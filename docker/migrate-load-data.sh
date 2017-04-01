feb4a4f964b8316d35e261ab895e4d565c1da87c
yusun0715 <yu.sun@polestarglobal.com>
2017-04-01 18:19:21 +0100

    load fixtures when running docker up

--- docker/migrate-load-data.sh
+++ docker/migrate-load-data.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 echo "Migrating database"
 python3 manage.py migrate --settings=sitemanagement.settings
+python3 manage.py loaddata megawatts/fixtures/load_sites.json --settings=sitemanagement.settings
